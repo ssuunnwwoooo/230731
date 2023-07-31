d28d091f179215228e9b7086e4d0dc88bd532c59
ssuunnwwoooo <o3olia@naver.com>
2023-07-31 18:17:40 +0900

    230731

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+        sct > 0
+            ? $('.header').addClass('on')
+            : $('.header').removeClass('on');
+    });
+
+    var main_slide = new Swiper(".main_slide", {
+        loop: true,
+    });
+
+})
